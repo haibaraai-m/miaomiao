d332094febad2e9be7da6b40d6412f79eb3ef7ca
haibaraai <mzx@gmail.com>
2020-09-15 20:50:24 +0800

    viewRouter init

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,19 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import MovieRouter from './movie'
+import MineRouter from './cinema'
+import CinemaRouter from './mine'
 
 Vue.use(VueRouter)
-
-const routes = [
-]
 
 const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
-  routes
+  routes:[
+    MovieRouter,
+    MineRouter,
+    CinemaRouter
+  ]
 })
 
 export default router
